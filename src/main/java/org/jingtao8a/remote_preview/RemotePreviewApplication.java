07acbcf7c7cfbe4eb0a4ef7636e1a6ef1e228e51
yuxintao <1921056015@qq.com>
2024-09-14 19:31:24 +0800

    init工程目录，制作remote-preview接口文件

---
+++ src/main/java/org/jingtao8a/remote_preview/RemotePreviewApplication.java
@@ -0,0 +1,13 @@
+package org.jingtao8a.remote_preview;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RemotePreviewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RemotePreviewApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/jingtao8a/remote_preview/RemotePreviewApplicationTests.java
@@ -0,0 +1,13 @@
+package org.jingtao8a.remote_preview;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RemotePreviewApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
