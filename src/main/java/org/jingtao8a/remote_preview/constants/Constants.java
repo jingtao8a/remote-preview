c9bb9aa94c30b8ab5d3f946b77186f87b413b8cd
yuxintao <1921056015@qq.com>
2024-09-15 09:28:47 +0800

    完成initApp插入数据

---
+++ src/main/java/org/jingtao8a/remote_preview/constants/Constants.java
@@ -0,0 +1,6 @@
+package org.jingtao8a.remote_preview.constants;
+
+public class Constants {
+    public static final Integer FILE_ID_LENGTH = 20;
+    public static final String ROOT_FILE_ID = "0";
+}
--- src/test/java/org/jingtao8a/remote_preview/RemotePreviewApplicationTests.java
+++ src/test/java/org/jingtao8a/remote_preview/RemotePreviewApplicationTests.java
@@ -1,13 +1,93 @@
 package org.jingtao8a.remote_preview;
 
+import org.jingtao8a.remote_preview.config.AppConfig;
+import org.jingtao8a.remote_preview.constants.Constants;
+import org.jingtao8a.remote_preview.entity.po.FileInfo;
+import org.jingtao8a.remote_preview.entity.query.FileInfoQuery;
+import org.jingtao8a.remote_preview.enums.FileTypeEnum;
+import org.jingtao8a.remote_preview.enums.FolderTypeEnum;
+import org.jingtao8a.remote_preview.enums.StatusEnum;
+import org.jingtao8a.remote_preview.mapper.FileInfoMapper;
+import org.jingtao8a.remote_preview.service.FileInfoService;
+import org.jingtao8a.remote_preview.utils.StringTools;
 import org.junit.jupiter.api.Test;
+import org.slf4j.*;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
 
-@SpringBootTest
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+import javax.annotation.Resource;
+
+@SpringBootTest(classes = {org.jingtao8a.remote_preview.RemotePreviewApplication.class})
 class RemotePreviewApplicationTests {
-
+	private Logger logger = LoggerFactory.getLogger(RemotePreviewApplicationTests.class);
+	@Resource
+	private AppConfig appConfig;
+	@Resource
+	private FileInfoService fileInfoService;
 	@Test
-	void contextLoads() {
-	}
+//	@Transactional(rollbackFor = Exception.class)
+	public void initApp() {
+		System.out.println(appConfig.getRemotePreviewMaterial());
+//		System.out.println(appConfig.getLogRootLevel());
+		File remotePreviewMaterialDir = new File(appConfig.getRemotePreviewMaterial());
+		if (!remotePreviewMaterialDir.exists()) {
+			logger.error("remotePreviewMaterial does not exist or is not a directory");
+			return;
+		}
+		if (!remotePreviewMaterialDir.isDirectory()) {
+			logger.error("remotePreviewMaterial is not a directory");
+			return;
+		}
+		logger.info("start scan remotePreviewMaterial");
+		List<FileInfo> fileInfoList = new ArrayList<>();
+		File[] fileList = remotePreviewMaterialDir.listFiles();
+		for (File file : fileList) {
+			FileInfo newFileInfo = new FileInfo();
+			String fileId = StringTools.getRandomString(Constants.FILE_ID_LENGTH);
+			newFileInfo.setFileId(fileId);
+			newFileInfo.setFilePid(Constants.ROOT_FILE_ID);
+			newFileInfo.setFileName(file.getName());
 
+			if (file.isDirectory()) {//目录
+				newFileInfo.setFolderType(FolderTypeEnum.FOLDER.getType());
+				newFileInfo.setStatus(StatusEnum.USING.getStatus());
+				fileInfoList.add(newFileInfo);
+				traverseDirectory(fileInfoList, file, fileId);
+			} else if (file.isFile()) {//文件
+				newFileInfo.setFileSize(file.getTotalSpace());
+				newFileInfo.setFolderType(FolderTypeEnum.FILE.getType());
+				newFileInfo.setFileType(FileTypeEnum.getFileTypeBySuffix(StringTools.getFileSuffix(file.getName())).getType());
+				newFileInfo.setStatus(StatusEnum.TRANSFER.getStatus());
+				fileInfoList.add(newFileInfo);
+			}
+		}
+//		System.out.println(fileInfoList);
+		fileInfoService.addBatch(fileInfoList);
+	}
+	void traverseDirectory(List<FileInfo> fileInfoList, File dir, String dirFileId) {
+		File[] fileList = dir.listFiles();
+		for (File file : fileList) {
+			FileInfo newFileInfo = new FileInfo();
+			String fileId = StringTools.getRandomString(Constants.FILE_ID_LENGTH);
+			newFileInfo.setFileId(fileId);
+			newFileInfo.setFilePid(dirFileId);
+			newFileInfo.setFileName(file.getName());
+
+			if (file.isDirectory()) {//目录
+				newFileInfo.setFolderType(FolderTypeEnum.FOLDER.getType());
+				newFileInfo.setStatus(StatusEnum.USING.getStatus());
+				fileInfoList.add(newFileInfo);
+				traverseDirectory(fileInfoList, file, fileId);
+			} else if (file.isFile()) {//文件
+				newFileInfo.setFileSize(file.getTotalSpace());
+				newFileInfo.setFolderType(FolderTypeEnum.FILE.getType());
+				newFileInfo.setFileType(FileTypeEnum.getFileTypeBySuffix(StringTools.getFileSuffix(file.getName())).getType());
+				newFileInfo.setStatus(StatusEnum.TRANSFER.getStatus());
+				fileInfoList.add(newFileInfo);
+			}
+		}
+	}
 }
