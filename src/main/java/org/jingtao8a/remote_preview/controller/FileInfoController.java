b23d4fe9e60566808164f4c8dce34693be8a4788
yuxintao <1921056015@qq.com>
2024-09-15 09:41:32 +0800

    完成/file/loadDataList接口

--- src/main/java/org/jingtao8a/remote_preview/controller/FileInfoController.java
+++ src/main/java/org/jingtao8a/remote_preview/controller/FileInfoController.java
@@ -3,6 +3,7 @@ import java.util.List;
 import org.jingtao8a.remote_preview.entity.po.FileInfo;
 import org.jingtao8a.remote_preview.entity.query.FileInfoQuery;
 import org.jingtao8a.remote_preview.service.FileInfoService;
+import org.jingtao8a.remote_preview.vo.PaginationResultVO;
 import org.jingtao8a.remote_preview.vo.ResponseVO;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,7 +14,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 @Date:2024-09-15
 */
 @RestController
-@RequestMapping("/fileInfo")
+@RequestMapping("/file")
 public class FileInfoController extends ABaseController {
 
 	@Resource
@@ -21,71 +22,10 @@ public class FileInfoController extends ABaseController {
 	/**
 	 * 分页查询
 	*/
-	@RequestMapping("loadDataList")
+	@RequestMapping("/loadDataList")
 	public ResponseVO loadDataList(FileInfoQuery query) {
-		return getSuccessResponseVO(fileInfoService.findListByPage(query));
-	}
-
-	/**
-	 * 新增
-	*/
-	@RequestMapping("add")
-	public ResponseVO add(FileInfo bean) {
-		fileInfoService.add(bean);
-		return getSuccessResponseVO(null);
-	}
-
-	/**
-	 * 新增/修改
-	*/
-	@RequestMapping("addOrUpdate")
-	public ResponseVO addOrUpdate(FileInfo bean) {
-		fileInfoService.addOrUpdate(bean);
-		return getSuccessResponseVO(null);
-	}
-
-	/**
-	 * 批量新增
-	*/
-	@RequestMapping("addBatch")
-	public ResponseVO addBatch(@RequestBody List<FileInfo> listBean) {
-		fileInfoService.addBatch(listBean);
-		return getSuccessResponseVO(null);
-	}
-
-	/**
-	 * 批量新增/修改
-	*/
-	@RequestMapping("addOrUpdateBatch")
-	 public ResponseVO addOrUpdateBatch(@RequestBody List<FileInfo> listBean) {
-		fileInfoService.addOrUpdateBatch(listBean);
-		return getSuccessResponseVO(null);
-	}
-
-	/**
-	 * 根据FileId查询
-	*/
-	@RequestMapping("getFileInfoByFileId")
-	public  ResponseVO getFileInfoByFileId(String fileId) {
-		return getSuccessResponseVO(fileInfoService.selectByFileId(fileId));
-	}
-
-	/**
-	 * 根据FileId更新
-	*/
-	@RequestMapping("updateFileInfoByFileId")
-	public ResponseVO updateFileInfoByFileId(FileInfo bean, String fileId) {
-		fileInfoService.updateByFileId(bean, fileId);
-		return getSuccessResponseVO(null);
-	}
-
-	/**
-	 * 根据FileId删除
-	*/
-	@RequestMapping("deleteFileInfoByFileId")
-	public ResponseVO deleteFileInfoByFileId(String fileId) {
-		fileInfoService.deleteByFileId(fileId);
-		return getSuccessResponseVO(null);
+		PaginationResultVO<FileInfo>  paginationResultVO = fileInfoService.findListByPage(query);
+		return getSuccessResponseVO(paginationResultVO);
 	}
 
 }
