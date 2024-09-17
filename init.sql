8451f9ba5a5aa15730ab0579ec8d2cb990e9be10
yuxintao <1921056015@qq.com>
2024-09-17 10:29:57 +0800

    将file_cover和file_path字段类型改为VARCHAR(300)

--- init.sql
+++ init.sql
@@ -9,7 +9,7 @@ CREATE TABLE `remote-preview`.`file_info` (
   `folder_type` INT NULL COMMENT '0:文件 1:目录',
   `file_type` INT NULL COMMENT '1:视频 2：音频 3：图片 4：pdf 5:doc  6:excel  7:txt 8:code 9:zip 10:其它',
   `status` INT NULL COMMENT '0:转码中 1：转码失败 2：转码成功',
-  `file_cover` VARCHAR(100) NULL COMMENT '封面（图片、视频）',
-  `file_path` VARCHAR(100) NULL COMMENT '文件路径',
+  `file_cover` VARCHAR(300) NULL COMMENT '封面（图片、视频）',
+  `file_path` VARCHAR(300) NULL COMMENT '文件路径',
   PRIMARY KEY (`file_id`),
   INDEX `idx_file_pid` (`file_pid` ASC) INVISIBLE);
